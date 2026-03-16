dfb380278349a3f24e0d424306d8f9844a2c18e7
Mahendran M <mahendran.m@impacteers.com>
2026-03-16 14:29:52 +0530

    js backend dev

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+app.get("/products", (req, res) => {
+  res.json([
+    { id: 1, name: "Laptop", price: 50000 },
+    { id: 2, name: "Mouse", price: 500 },
+  ]);
+});
+
+app.listen(3000, () => {
+  console.log("The server is running");
+});
